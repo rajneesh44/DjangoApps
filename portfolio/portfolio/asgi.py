529e99287ee9f6a4591ec110468d96737c9b03e5
rajneesh44 <rajneeshsharma338@gmail.com>
2020-03-22 01:51:36 +0530

    portfolio app

--- portfolio/portfolio/asgi.py
+++ portfolio/portfolio/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for portfolioProject project.
+ASGI config for portfolio project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolioProject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
 
 application = get_asgi_application()
