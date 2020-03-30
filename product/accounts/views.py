32385f8ce3a37b6fcb84496dd6948593e6e52028
rajneesh44 <rajneeshsharma338@gmail.com>
2020-03-31 01:01:51 +0530

    login logout added

--- product/accounts/views.py
+++ product/accounts/views.py
@@ -22,9 +22,20 @@ def signup(request):
         # user wants to enter info
         return render(request, 'accounts/signup.html')
 
+
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method=='POST':
+        user = auth.authenticate(username=request.POST['username'],password=request.POST['password'])
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'accounts/login.html', {'error': 'Username or password is incorrect'})
+    else:
+        return render(request, 'accounts/login.html')
 
 def logout(request):
     #need to route to home page
-    return render(request, 'accounts/signup.html')
+    if request.method=='POST':
+        auth.logout(request)
+        return redirect('home')
